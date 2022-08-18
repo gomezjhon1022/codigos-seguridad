91b731a3b01a37715fd5fc7cde0bb4adc305b129
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-08-17 22:49:17 -0500

    Estados simples: React.Component vs React.useState

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { ClassState } from './ClassState.js';
 function App() {
   return (
     <div className="App">
-      <UseState />
-      <ClassState />
+      <UseState name="UseState"/>
+      <ClassState name="ClassState"/>
     </div>
   );
 }
--- src/UseState.js
+++ src/UseState.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-function UseState() {
+function UseState({name}) {
+  const [error, setError] = React.useState(false);
   return (
     <div>
-      <h2>Eliminar UseState</h2>
-      <p>Por favor, escribe el código de seguridad</p>
+      <h2>Eliminar {name}</h2>
+      <p>Por favor, escr  ibe el código de seguridad</p>
+      {error && (
+        <p>Error: el código es incorrecto</p>
+      )}
       <input placeholder='Código de seguridad'/>
-      <button>Comprobar</button>
+      <button
+        onClick={() =>setError(!error)}
+      >Comprobar</button>
     </div>
   )
 }
