b080c785f864777a5e3270c402609e6ef4c2e6bc
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-08-18 10:55:20 -0500

    Estados independientes con useState

--- src/UseState.js
+++ src/UseState.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
+const SECURITY_CODE = 'paradigma';
 function UseState({name}) {
-  const [error, setError] = React.useState(true);
+  const [error, setError] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
+  const [value, setValue] = React.useState('');
+  console.log(value);
   React.useEffect(() => {
     console.log("Empexando el efecto")
     if (loading) {
       setTimeout(() => {
         console.log("Haciendo la validacion")
+        if (value !== SECURITY_CODE) {
+          setError(true);
+        }
         setLoading(false);
+        
         console.log("Terminando la validacion")
       }, 3000);
     }
@@ -24,9 +31,19 @@ function UseState({name}) {
       {loading && (
         <p>Cargando ...</p>
       )}
-      <input placeholder='Código de seguridad'/>
+      <input 
+        placeholder='Código de seguridad'
+        value= {value}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+
+      />
       <button
-        onClick={() =>setLoading(true)}
+        onClick={() =>{
+          setLoading(true);
+          setError(false);
+        }}
       >Comprobar</button>
     </div>
   )
